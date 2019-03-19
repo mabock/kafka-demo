6075c345117a653267950598a54c1e40d7b9c4ef
Maximilian Bock <maximilian.bock@akelius.de>
2019-03-19 10:00:07 +0100

    initial commit with basic spring generated files

---
+++ src/test/kotlin/de/bock/coding/kafkaexample/KafkaExampleApplicationTests.kt
@@ -0,0 +1,16 @@
+package de.bock.coding.kafkaexample
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KafkaExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
